711ba091c691c73edfc0dd4089726051d7183361
Dmytro Dovbnia <dmytro.dovbnia@gmail.com>
2017-11-03 10:16:54 +0200

    fix ufter marge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,7 +29,6 @@ import {LoginPage} from "../pages/login/login";
 import { SearchByParamPage} from "../pages/searchByParam/searchByParam";
 import {SearchByParamForm} from "../forms/searchByParam/searchByParam";
 //import { HTTP } from '@ionic-native/http';
-import {HttpModule} from '@angular/http';
 
 
 @NgModule({
@@ -46,7 +45,7 @@ import {HttpModule} from '@angular/http';
     SeancePage,
     RegistrationPage,
     RegistrationForm,
-    
+
     LoginForm,
     LoginPage,
     SearchByParamPage,
@@ -67,7 +66,7 @@ import {HttpModule} from '@angular/http';
     FilmName,
     Test,
     SeancePage,
-    RegistrationPage
+    RegistrationPage,
     LoginPage,
     SearchByParamPage
 
